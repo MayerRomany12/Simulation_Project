8433d766a63f0d2899d601e97eccffce141648a0
Your Name <you@example.com>
2026-05-06 16:53:48 +0300

    Update baseURL to production server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://simulation-api-seven.vercel.app'
 });
 
 export const runSimulation = async (params) => {
